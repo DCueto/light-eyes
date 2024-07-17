f638f35c8e719ad829ce3089d975f5a1f8c5de3e
julio <jcsmerubia@gmail.com>
2024-07-17 09:05:55 +0200

    new git ignore

--- light-eyes/DTO/Account/NewUserDto.cs
+++
@@ -1,8 +0,0 @@
-namespace light_eyes.DTO.Account;
-
-public class NewUserDto
-{
-    public string UserName { get; set; }
-    public string Email { get; set; }
-    public string Token { get; set; }
-}
