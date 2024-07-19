8fc93bc949326ca57b2a583190b68238b51a276a
limematheuz <mribeiro17.info@gmail.com>
2024-07-19 13:41:41 +0200

    testing repository initial configuration

---
+++ Testing/RepositoryTests/CheckListTests.cs
@@ -0,0 +1,20 @@
+﻿using light_eyes.Data;
+using light_eyes.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Testing.RepositoryTests;
+
+public class CheckListTests
+{
+    private readonly AppDbContext _context;
+    private readonly Mock<ICheckListRepository> _checkListMock;
+
+    public CheckListTests()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "")
+            .Options;
+        _checkListMock = new Mock<ICheckListRepository>();
+    }
+}
