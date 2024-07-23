886f480145b5e3bed9db76f3e1f360a20ab4c242
Daniel Cueto <dcueto14.info@gmail.com>
2024-07-23 20:29:38 +0200

    Included List of ChecklistItemOption to ChecklistItem repository methods

--- light-eyes/Repositories/CheckListItemRepository.cs
+++ light-eyes/Repositories/CheckListItemRepository.cs
@@ -17,12 +17,16 @@ public class CheckListItemRepository : ICheckListItemRepository
     
     public async Task<List<CheckListItem>> GetAllAsync()
     {
-        return await _context.CheckListItem.ToListAsync();
+        return await _context.CheckListItem
+            .Include(c => c.CheckListItemOptions)
+            .ToListAsync();
     }
 
     public async Task<CheckListItem?> GetByIdAsync(int id)
     {
-        return await _context.CheckListItem.FindAsync(id);
+        return await _context.CheckListItem
+            .Include(c => c.CheckListItemOptions)
+            .FirstOrDefaultAsync(c => c.CheckListItemId == id);
     }
 
     public async Task<CheckListItem> CreateAsync(CheckListItem checkModel)
