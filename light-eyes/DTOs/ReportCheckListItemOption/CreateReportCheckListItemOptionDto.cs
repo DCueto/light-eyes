b34a73d3096b26d5c465029650cb73f1a97371c5
Daniel Cueto <dcueto14.info@gmail.com>
2024-07-25 20:27:51 +0200

    ReportCheckListItemOption Dtos done

--- light-eyes/DTOs/ReportCheckListItemOption/CreateReportCheckListItemOptionDto.cs
+++ light-eyes/DTOs/ReportCheckListItemOption/CreateReportCheckListItemOptionDto.cs
@@ -2,5 +2,6 @@ namespace light_eyes.DTOs.ReportCheckListItemOption;
 
 public class CreateReportCheckListItemOptionDto
 {
-    
+    public int CheckListItemOptionId { get; set; }
+    public bool IsSelected { get; set; } = false;
 }
--- light-eyes/DTOs/ReportCheckListItemOption/UpdateReportCheckListItemOptionDto.cs
+++ light-eyes/DTOs/ReportCheckListItemOption/UpdateReportCheckListItemOptionDto.cs
@@ -2,5 +2,6 @@ namespace light_eyes.DTOs.ReportCheckListItemOption;
 
 public class UpdateReportCheckListItemOptionDto
 {
-    
+    public int Id { get; set; }
+    public bool IsSelected { get; set; }
 }
