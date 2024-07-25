74bbcbf88b18be746dc24b60af6e6dfa8829e540
Daniel Cueto <dcueto14.info@gmail.com>
2024-07-25 12:32:58 +0200

    DbSet properties into DbContext for new entities

--- light-eyes/Data/AppDbContext.cs
+++ light-eyes/Data/AppDbContext.cs
@@ -9,10 +9,16 @@ public class AppDbContext : IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions options) : base(options){}
 
-    public DbSet<Report> Report { get; set; }
     public DbSet<CheckList> CheckList { get; set; }
     public DbSet<CheckListItem> CheckListItem { get; set; }
     public DbSet<CheckListItemOption> CheckListItemOption { get; set; }
+    
+    public DbSet<Report> Report { get; set; }
+    public DbSet<ReportControlData> ReportControlData { get; set; }
+    public DbSet<ReportCheckListItem> ReportCheckListItems { get; set; }
+    public DbSet<ReportCheckListItemOption> ReportCheckListItemOptions { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
