f79de2d9e425d2363314aae042a7ce77c65d5c85
Daniel Cueto <dcueto14.info@gmail.com>
2024-07-25 20:33:12 +0200

    ReportControlData Dtos done

--- light-eyes/DTOs/ReportControlData/ReportControlDataDto.cs
+++ light-eyes/DTOs/ReportControlData/ReportControlDataDto.cs
@@ -2,5 +2,14 @@ namespace light_eyes.DTOs.ReportControlData;
 
 public class ReportControlDataDto
 {
+    public int Id { get; set; }
+
+    public DateTime ReviewDate { get; set; }
+    public string CreatedBy { get; set; }
+    public string ValidatedBy { get; set; }
+    public string ReviewedBy { get; set; }
+    public string DocumentCode { get; set; }
+    public string Department { get; set; }
     
+    public int ReportId { get; set; }
 }
--- light-eyes/DTOs/ReportControlData/UpdateReportControlDataDto.cs
+++ light-eyes/DTOs/ReportControlData/UpdateReportControlDataDto.cs
@@ -2,5 +2,12 @@ namespace light_eyes.DTOs.ReportControlData;
 
 public class UpdateReportControlDataDto
 {
+    public int Id { get; set; }
     
+    public DateTime ReviewDate { get; set; }
+    public string CreatedBy { get; set; }
+    public string ValidatedBy { get; set; }
+    public string ReviewedBy { get; set; }
+    public string DocumentCode { get; set; }
+    public string Department { get; set; }
 }
