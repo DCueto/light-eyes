5db4c3d6e7c8397d1aca74d67cab98b24bb0a64d
Daniel Cueto <dcueto14.info@gmail.com>
2024-07-25 12:24:27 +0200

    ReportControlData Entity done

--- light-eyes/Models/ReportCheckListItem.cs
+++ light-eyes/Models/ReportCheckListItem.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace light_eyes.Models;
 
 public class ReportCheckListItem
 {
+    [Key]
     public int Id { get; set; }
     
     [ForeignKey("Report")]
--- light-eyes/Models/ReportControlData.cs
+++ light-eyes/Models/ReportControlData.cs
@@ -1,6 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace light_eyes.Models;
 
 public class ReportControlData
 {
+    [Key]
+    public int Id { get; set; }
+
+    public DateTime ReviewDate { get; set; }
     
+    public string CreatedBy { get; set; } = String.Empty;
+    public string ValidatedBy { get; set; } = String.Empty;
+    public string ReviewedBy { get; set; } = String.Empty;
+    public string DocumentCode { get; set; } = String.Empty;
+    public string Department { get; set; } = String.Empty;
+    
+    [ForeignKey("Report")]
+    public int ReportId { get; set; }
+    public Report Report { get; set; }
 }
