5b6c62f50289b4ef793d314764206591c6206840
Daniel Cueto <dcueto14.info@gmail.com>
2024-07-25 12:29:29 +0200

    Client entity done

--- light-eyes/Models/Client.cs
+++ light-eyes/Models/Client.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 namespace light_eyes.Models;
 
 public class Client
 {
-    
+    [Key] 
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string CompanyName { get; set; }
+    public string Area { get; set; }
+    public string ContactEmail { get; set; }
+    public string ContactPhone { get; set; }
+
+    public List<Report> Reports { get; set; } // Be careful with cycle between Report and Client
 }
